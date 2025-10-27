7c16cc160d6a06c0810c468f9a3d13fbed9f3f03
TidosDK <git@msnn.dev>
2025-10-27 12:05:55 +0100

    Added initial Terraform providers

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.33"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "big-data-group"
+}
